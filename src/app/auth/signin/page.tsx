75eef29b2628d56854a8d0bd8afe11681e5dbaf0
zayyid123 <mochamadzayyid@gmail.com>
2024-11-16 22:09:15 +0700

    style: fix UI signin and signup

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
   title: "Blogging - For Blog Only",
